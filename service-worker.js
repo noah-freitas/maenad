2b789b9c49e612f98d132de7ff3001f3ba696847
Noah Freitas <noah.j.freitas@gmail.com>
2016-05-08 16:41:24 -0700

    Add Service Worker with static resource caching.

---
+++ service-worker.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const DEBUG = false;
+
+const cacheName = 'app-assets-0.3.0';
+
+const assetUrls = [
+    'bower_components/aws-sdk.js',
+    'bower_components/document-register-element.js',
+    'bower_components/system.js',
+    'components/maenad-aws-importer/js/maenad-aws-importer.js',
+    'components/maenad-aws-song-importer/css/maenad-aws-song-importer.css',
+    'components/maenad-aws-song-importer/js/maenad-aws-song-importer.js',
+    'components/maenad-configuration-manager/css/maenad-configuration-manager.css',
+    'components/maenad-configuration-manager/js/maenad-configuration-manager.js',
+    'components/maenad-loader/js/maenad-loader.js',
+    'components/maenad-loader/js/template.js',
+    'components/maenad-player/css/maenad-player.css',
+    'components/maenad-player/js/maenad-player.js',
+    'components/maenad-song-item/css/maenad-song-item.css',
+    'components/maenad-song-item/js/maenad-song-item.js',
+    'components/maenad-song-list/js/maenad-song-list.js',
+    'css/default-theme/main.css',
+    'js/config.js',
+    'js/index.js',
+    'js/load.js',
+    'js/play.js',
+    'lib/config.js',
+    'lib/data.js',
+    'lib/db-main.js',
+    'lib/db.js',
+    'lib/id3-43081j.js',
+    'lib/register-element.js',
+    'lib/s3.js',
+    'lib/service-worker-helpers.js',
+    'lib/song.js',
+    'lib/sw.js',
+    'config.html',
+    'index.html',
+    'load.html',
+    'play.html',
+    '/'
+];
+
+self.addEventListener('install'  , installHandler);
+self.addEventListener('activate' , activateHandler);
+self.addEventListener('fetch'    , fetchHandler);
+
+// installHandler :: ExtendableEvent -> undefined
+function installHandler(e) {
+    log('install: ', e);
+
+    e.waitUntil(caches.open(cacheName)
+                      .then(cache => cache.addAll(assetUrls)));
+}
+
+// activateHandler :: ExtendableEvent -> undefined
+function activateHandler(e) {
+    log('activate: ', e);
+
+    e.waitUntil(caches.keys()
+                      .then(cacheList => Promise.all(cacheList.filter(c => c !== cacheName)
+                                                              .map(c => caches.delete(c)))));
+}
+
+// fetchHandler :: FetchEvent -> undefined
+function fetchHandler(e) {
+    log('fetch: ', e);
+
+    e.respondWith(respondFromCacheOrNetwork(e));
+}
+
+// log :: ...String -> undefined
+function log(...msg) {
+    if (DEBUG) console.apply(console, msg);
+}
+
+// respondFromCacheOrNetwork :: FetchEvent -> Promise<Response>
+function respondFromCacheOrNetwork(e) {
+    return caches.open(cacheName)
+                 .then(cache => cache.match(e.request))
+                 .then(logCacheResult)
+                 .then(response => response || fetch(e.request));
+
+    // logCacheResult :: Response || undefined -> Response || undefined
+    function logCacheResult(response) {
+        log(e.request.method, ' ', e.request.url, ' served from ', response ? 'cache' : 'network');
+
+        return response;
+    }
+}
